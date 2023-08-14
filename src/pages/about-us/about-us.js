cb80e9a518d9101d53a25eea676a184304c3b133
Alex Hrabovyi <alexgraboviypro@gmail.com>
2023-08-14 15:46:08 +0300

    Create and setup files for about-us page

---
+++ src/pages/about-us/about-us.js
@@ -0,0 +1,7 @@
+import setupCopyTelButton from '../../blocks/header/_header.js';
+import setupMainNav from '../../blocks/main-nav/_main-nav.js';
+import footerAddEmailValidation from '../../blocks/footer/_footer.js';
+
+setupCopyTelButton();
+setupMainNav();
+footerAddEmailValidation();
