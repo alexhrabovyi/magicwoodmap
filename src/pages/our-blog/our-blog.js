d24ddca3a321f921f2286cbf7abca83a9811b015
Alex Hrabovyi <alexgraboviypro@gmail.com>
2023-08-16 17:02:04 +0300

    Create initial files for our blog page

---
+++ src/pages/our-blog/our-blog.js
@@ -0,0 +1,11 @@
+import setupCopyTelButton from '../../blocks/header/_header.js';
+import setupMainNav from '../../blocks/main-nav/_main-nav.js';
+import setupBreadcrumbs from '../../blocks/breadcrumbs/_breadcrumbs.js';
+import setupCallBackForm from '../../blocks/call-back-form/call-back-form.js';
+import setupFooterForm from '../../blocks/footer/_footer.js';
+
+setupCopyTelButton();
+setupMainNav();
+setupBreadcrumbs([['Головна', '#'], ['Блог', '#']]);
+setupCallBackForm();
+setupFooterForm();
