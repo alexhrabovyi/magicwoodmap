86cc23df4a0b3abdfd77c90c04f37b5d4d449d4a
Alex Hrabovyi <alexgraboviypro@gmail.com>
2023-08-24 17:28:59 +0300

    Create all js code for range separate work

--- src/blocks/catalog/_catalog.js
+++ src/blocks/catalog/_catalog.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable max-classes-per-file */
 export default function setupCatalog() {
   class Select {
@@ -21,7 +22,10 @@ export default function setupCatalog() {
 
       for (let i = 0; i < this.selectButtons.length; i += 1) {
         this.selectButtons[i].setAttribute('data-select-btn-id', i);
-        if (i === this.selectedId) this.selectButtons[i].classList.add(this.selectedButtonClass);
+        if (i === this.selectedId) {
+          this.selectButtons[i].classList.add(this.selectedButtonClass);
+          this.selectChosenText.textContent = this.selectButtons[i].textContent;
+        }
       }
 
       this.select.addEventListener('click', this.clickHandle.bind(this), { passive: true });
@@ -107,13 +111,149 @@ export default function setupCatalog() {
     }
 
     setup() {
-      this.buttonLeft = this.rangeBlock.querySelector('[data-range-btn-left]');
-      this.buttonRight = this.rangeBlock.querySelector('[data-range-btn-right]');
+      this.rangeLine = this.rangeBlock.querySelector('.catalog__range-line');
+      this.buttonMin = this.rangeBlock.querySelector('#range-btn-min');
+      this.buttonMax = this.rangeBlock.querySelector('#range-btn-max');
+
+      this.buttonMin.addEventListener('mousedown', this.onMouseDown.bind(this, 'min'));
+      this.buttonMax.addEventListener('mousedown', this.onMouseDown.bind(this, 'max'));
+
+      this.setupInput();
 
       setTimeout(() => {
-        this.lineWidth = this.rangeBlock.offsetWidth;
-        console.log(this.lineWidth);
+        this.buttonMax.style.left = `${this.rangeLineWidth - this.buttonWidth}px`;
+        this.buttonMin.style.left = '0';
       }, 0);
+    }
+
+    setupInput() {
+      this.inputMin = this.rangeBlock.querySelector('#range-input-min');
+      this.inputMax = this.rangeBlock.querySelector('#range-input-max');
+
+      this.inputMin.addEventListener('focusout', this.onFocusOut.bind(this, 'min'), { passive: true });
+      this.inputMax.addEventListener('focusout', this.onFocusOut.bind(this, 'max'), { passive: true });
+    }
+
+    onFocusOut(inputType, e) {
+      const input = e.target.closest('input');
+
+      if (input.value.length === 0) {
+        input.value = input[inputType];
+      }
+
+      if (inputType === 'min') {
+        if (+input.value < +input.min) input.value = input.min;
+        if (+input.value >= +this.inputMax.value) input.value = this.inputMax.value;
+      } else if (inputType === 'max') {
+        if (+input.value > +input.max) input.value = input.max;
+        if (+input.value < +this.inputMin.value) input.value = this.inputMin.value;
+      }
+
+      input.value = (+input.value).toFixed(0);
+      const { value } = input;
+
+      this.renderButton(value, inputType);
+    }
+
+    onMouseDown(type, e) {
+      e.preventDefault();
+      const button = e.target.closest('.catalog__range-button');
+
+      const shiftX = e.clientX - this.getElementLeftCoord(button);
+
+      button.style.transitionDuration = '0s';
+
+      const onMouseMoveModified = this.onMouseMove.bind(this, type, button, shiftX);
+      document.addEventListener('mousemove', onMouseMoveModified);
+      document.addEventListener('mouseup', this.onMouseUp.bind(this, button, onMouseMoveModified), { once: true });
+    }
+
+    onMouseMove(type, button, shiftX, event) {
+      const rangeLineLeftCoord = this.getElementLeftCoord(this.rangeLine);
+
+      let buttonShiftLeft = event.clientX - rangeLineLeftCoord - shiftX;
+
+      if (type === 'min') {
+        const buttonMaxLeftShift = this.getElementStyleLeft(this.buttonMax);
+
+        if (buttonShiftLeft <= 0) {
+          buttonShiftLeft = 0;
+        } else if (buttonShiftLeft >= buttonMaxLeftShift - this.buttonWidth) {
+          buttonShiftLeft = buttonMaxLeftShift - this.buttonWidth;
+        }
+      } else if (type === 'max') {
+        const buttonMinLeftShift = this.getElementStyleLeft(this.buttonMin);
+
+        if (buttonShiftLeft >= this.rangeLineWidth - this.buttonWidth) {
+          buttonShiftLeft = this.rangeLineWidth - this.buttonWidth;
+        } else if (buttonShiftLeft <= buttonMinLeftShift + this.buttonWidth) {
+          buttonShiftLeft = buttonMinLeftShift + this.buttonWidth;
+        }
+      }
+      button.style.left = `${buttonShiftLeft}px`;
+      this.calcInputs(buttonShiftLeft, type);
+    }
+
+    onMouseUp(button, mouseMoveFunc) {
+      button.style.transitionDuration = '';
+      document.removeEventListener('mousemove', mouseMoveFunc);
+    }
+
+    calcInputs(shiftLeft, type) {
+      const minValue = +this.inputMin.min;
+      const maxValue = +this.inputMin.max;
+
+      let percent;
+
+      if (type === 'min') {
+        const rightShift = this.rangeLineWidth - shiftLeft - this.buttonWidth * 2;
+        percent = rightShift / (this.rangeLineWidth - this.buttonWidth * 2);
+        percent = Math.abs(percent - 1);
+      } else if (type === 'max') {
+        shiftLeft -= this.buttonWidth;
+        percent = shiftLeft / (this.rangeLineWidth - this.buttonWidth * 2);
+      }
+
+      percent = +percent.toFixed(2);
+      const inputValue = (((maxValue - minValue) * percent) + minValue).toFixed(0);
+
+      if (type === 'min') {
+        this.inputMin.value = inputValue;
+      } else if (type === 'max') {
+        this.inputMax.value = inputValue;
+      }
+    }
+
+    renderButton(value, inputType) {
+      const minValue = +this.inputMin.min;
+      const maxValue = +this.inputMin.max;
+      const percent = +((value - minValue) / (maxValue - minValue)).toFixed(2);
+
+      if (inputType === 'min') {
+        const leftShift = (this.rangeLineWidth - this.buttonWidth * 2) * percent;
+        this.buttonMin.style.left = `${leftShift}px`;
+      } else if (inputType === 'max') {
+        let leftShift = (this.rangeLineWidth - this.buttonWidth * 2) * percent;
+        leftShift += this.buttonWidth;
+        this.buttonMax.style.left = `${leftShift}px`;
+      }
+    }
+
+    getElementLeftCoord(elem) {
+      const { left } = elem.getBoundingClientRect();
+      return left;
+    }
+
+    getElementStyleLeft(elem) {
+      return +elem.style.left.match(/\d+(\.\d+)?/)[0];
+    }
+
+    get buttonWidth() {
+      return this.buttonMin.offsetWidth;
+    }
+
+    get rangeLineWidth() {
+      return this.rangeLine.offsetWidth;
     }
   }
 
