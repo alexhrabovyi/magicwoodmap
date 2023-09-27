3af64d58d92b60f33caf36b726ffc5f3fb5b86cb
Alex Hrabovyi <alexgraboviypro@gmail.com>
2023-09-27 14:43:08 +0300

    Create initial files for payment-and-delivery page

---
+++ src/pages/payment-and-delivery/payment-and-delivery.js
@@ -0,0 +1,13 @@
+import setupCopyTelButton from '../../blocks/header/_header.js';
+import setupMainNav from '../../blocks/main-nav/_main-nav.js';
+import setupBreadcrumbs from '../../blocks/breadcrumbs/_breadcrumbs.js';
+import setupBagAndWishlistMenu from '../../blocks/bag-and-wishlist-menu/_bag-and-wishlist.js';
+import setupCallBackForm from '../../blocks/call-back-form/_call-back-form.js';
+import setupFooterForm from '../../blocks/footer/_footer.js';
+
+setupCopyTelButton();
+setupMainNav();
+setupBreadcrumbs([['Головна', '#'], ['Оплата та доставка ', '#']]);
+setupBagAndWishlistMenu();
+setupCallBackForm();
+setupFooterForm();
